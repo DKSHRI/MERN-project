1d4136df5945bed1e4c9ca0166a334c526c3f686
DKSHRI <devanshu.shrivastava@s.amity.edu>
2025-12-11 14:09:20 +0530

    bug update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './Component/Common/Header'
-import Footer from './Component/Common/footer'
+import Footer from './Component/Common/Footer'
 
 const App = () => {
   return (
